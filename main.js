3ee0921e8330945a99e19353da8778975d9e2581
albycc <richardwalton89@gmail.com>
2021-10-14 13:19:37 +0200

    added commentary in main.js
    added fish

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 let cat = "Felix";
 let dog = "Jake";
 let bird = "Skrawk";
+let fish = "sharky";
 const display = document.getElementById("animal-display");
 
+//create and add animals on list
 const animalList = [];
 animalList.push(cat);
 animalList.push(dog);
 animalList.push(bird);
 
+
+//printing out animals in index
 animalList.forEach(x =>{
     let para = document.createElement("p");
     para.textContent = x;
